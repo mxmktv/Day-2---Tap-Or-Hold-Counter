0125f8dfda0c6350e5eb74a96cf670c485c2373e
Максим Кузнецов <mxmktv@gmail.com>
2023-05-04 11:16:36 +0700

    Day 2 project

--- Day 2 - Tap Or Hold Counter/ViewController.swift
+++ Day 2 - Tap Or Hold Counter/ViewController.swift
@@ -1,19 +1,50 @@
 //
 //  ViewController.swift
-//  Day 2 - Tap Or Hold Counter
+//  Day 1 - Tap Counter
 //
-//  Created by Eastwood on 04.05.2023.
+//  Created by Eastwood on 29.04.2023.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
+    var counter = 0
+    var timer: Timer?
+    var lastPressTime = Date().timeIntervalSince1970
+
+    @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var counterLabel: UILabel!
+    @IBOutlet weak var longPressGesture: UILongPressGestureRecognizer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.button.addGestureRecognizer(longPressGesture)
     }
 
+    @IBAction func longPressGestureAction(_ sender: UILongPressGestureRecognizer) {
+        if sender.state == .began {
+            updateCounter()
+            timer = Timer.scheduledTimer(withTimeInterval: 0.3, repeats: true) { [weak self] _ in
+                            self?.updateCounter()
+            }
+        } else if sender.state == .ended {
+            timer?.invalidate()
+            timer = nil
+        }
+    }
 
+    @IBAction func tapButton(_ sender: UIButton) {
+        updateCounter()
+    }
+
+    @IBAction func resetBarItem(_ sender: UIBarButtonItem) {
+        counter = 0
+        counterLabel.text = String(counter)
+    }
+
+    func updateCounter() {
+        counter += 1
+        counterLabel.text = String(counter)
+    }
 }
 
