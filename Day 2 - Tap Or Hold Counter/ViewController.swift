c96c394d802108db72271643ada7c6416d7c8e8d
Максим Кузнецов <mxmktv@gmail.com>
2023-05-04 11:13:05 +0700

    Initial Commit

---
+++ Day 2 - Tap Or Hold Counter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Day 2 - Tap Or Hold Counter
+//
+//  Created by Eastwood on 04.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
